0f99876830eb487fc16d4861e33a40f3bed795e9
Ilya <zetLight5@gmail.com>
2024-12-14 18:04:09 +0300

    Save remote changes in GitProjectClass on development branch

--- src/main/java/GitProjectClass.java
+++ src/main/java/GitProjectClass.java
@@ -1,7 +1,7 @@
 public class GitProjectClass {
     public static void main(String[] args) {
         System.out.println("There is Git Project class (development version) (followed by remote developer)");
-        System.out.println("Changes from remote developer");
+        System.out.println("Changes from remote developer (saved on development version)");
         System.out.println("Changes from local developer");
         System.out.println("Advanced business logic from development branch");
     }
