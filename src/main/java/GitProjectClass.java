73c3b8660eb2d0946321c1f1d3e0ead3c9ad937d
Ilya <zetLight5@gmail.com>
2024-12-14 17:20:34 +0300

    Add Changes to GitProjectClass

--- src/main/java/GitProjectClass.java
+++ src/main/java/GitProjectClass.java
@@ -1,5 +1,6 @@
 public class GitProjectClass {
     public static void main(String[] args) {
         System.out.println("There is Git Project class (followed by remote developer)");
+        System.out.println("Changes from local developer");
     }
 }
