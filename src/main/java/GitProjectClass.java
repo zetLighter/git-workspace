3d9ea2edecd562b3603cdc3874c42d1bab1b6076
zetLighter <127424545+zetLighter@users.noreply.github.com>
2024-12-14 17:03:50 +0300

    Update GitProjectClass.java
    Add follow sign to GitProjectClass

--- src/main/java/GitProjectClass.java
+++ src/main/java/GitProjectClass.java
@@ -1,5 +1,5 @@
 public class GitProjectClass {
     public static void main(String[] args) {
-        System.out.println("There is Git Project class");
+        System.out.println("There is Git Project class (followed by remote developer)");
     }
 }
